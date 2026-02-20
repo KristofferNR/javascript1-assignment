76889eec39ba34307a7a4d082c91fd66af0200d9
KristofferNR <besohackzz@gmail.com>
2026-02-20 13:01:28 +0100

    add fetch function for getting products

--- index.js
+++ index.js
@@ -1,19 +1,42 @@
 async function loadProducts() {
 
     try {
+        //hämtar api till variablen response
         const response = await fetch('https://fakestoreapi.com/products');
+        //parsar response till json och lägger till i products
         const products = await response.json();
+        //hämtar bento diven med id
         const bentoContainer = document.getElementById('bentoContainer');
 
+        //går igenom alla products och printar ut dom till bentoContainer div
         products.forEach((product) => {
+            //skapar en div som är kopplad till productCard
             const productCard = document.createElement('div');
+            //ger alla nya div class="product-card"
             productCard.classList.add('product-card')
-            productCard.innerHTML = `
-                <img src="${product.image}" alt="${product.title}">
-                <h3>${product.title}</h3>
-                <p>${product.description}</p>
-                <p>Price: $${product.price}</p>
-            `;
+            //detta pushas in i product-card diven
+            //ÄNDRA innerHTML till textContent
+            //productCard.innerHTML = `
+                //<img src="${product.image}" alt="${product.title}">
+                //<h3>${product.title}</h3>
+                //<p>${product.description}</p>
+                //<p>Price: $${product.price}</p>
+            //`;
+
+            const productImg = document.createElement("img")
+            const productTitle = document.createElement("h3")
+            const productDescription = document.createElement("p")
+            const productPrice = document.createElement("p")
+
+            productImg.textContent = product.image
+            productTitle.textContent = product.title
+            productDescription.textContent = product.description
+            productPrice.textContent = product.price
+
+            //kopplar productCard till bentoContainern och gör productCard till ett child av bentoContainer
+            // <div id="bentoContainer">
+            //      <div class="product-card"></div>
+            // </div>
             bentoContainer.appendChild(productCard);
             
         });
@@ -42,5 +65,6 @@ async function addProducts() {
     }
 }
 
-loadProducts();
 addProducts();
+loadProducts();
+
