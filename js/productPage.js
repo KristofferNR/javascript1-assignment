7de2f9fcfe5f2d8ad7063b46255c9bc2f47455b0
one <one@url.com>
2026-02-26 20:48:57 +0100

    changes after user test

--- js/productPage.js
+++ js/productPage.js
@@ -45,19 +45,56 @@ addBotton.addEventListener("mouseup", () => {
 
 colors.addEventListener("click", (event) => {
   if (event.target.classList.contains("color-Choice")) {
-    const allLi = colors.querySelectorAll('li');
-        allLi.forEach(li => {
-            li.classList.remove('selected');
-        });
-    event.target.classList.add('selected');
+    const allLi = colors.querySelectorAll("li");
+    allLi.forEach((li) => {
+      li.classList.remove("selected");
+    });
+    event.target.classList.add("selected");
   }
 });
 colorsSmall.addEventListener("click", (event) => {
   if (event.target.classList.contains("color-Choice")) {
-    const allLi = colorsSmall.querySelectorAll('li');
-        allLi.forEach(li => {
-            li.classList.remove('selected');
-        });
-    event.target.classList.add('selected');
+    const allLi = colorsSmall.querySelectorAll("li");
+    allLi.forEach((li) => {
+      li.classList.remove("selected");
+    });
+    event.target.classList.add("selected");
   }
 });
+
+addImg(
+  document.getElementById("mainProductImg"),
+  "/imges/Group-13.png",
+  "Description of image",
+);
+// addImg(
+//   document.getElementById("small-productImg"),
+//   "/imges/Group-13.png",
+//   "Description of image",
+// );
+
+//En åternvändbar funktion för att lägga till bilder i divarna som jag skapat som placeholders
+function addImg(continer, imgSrc, imgAlt) {
+  const img = document.createElement("img");
+  img.src = imgSrc;
+  img.alt = imgAlt;
+  continer.style.position = "relative";
+
+  img.style.width = "100%";
+  img.style.height = "100%";
+  img.style.display = "block";
+  
+  img.style.objectFit = "cover";
+
+  continer.appendChild(img);
+}
+
+addMoreLike(document.getElementById("more-like-continer"));
+//First prototype for adding the right imges in more like.
+function addMoreLike(continer) {
+  const allDiv = continer.querySelectorAll("div");
+  allDiv.forEach((div) => {
+    addImg(div, "/imges/Group-9.png",
+  "Description of image");
+  });
+}
