6871706bdc773e3566f42b4447f6a50e4248e48d
one <one@url.com>
2026-02-26 15:34:11 +0100

    added some functions for respons on click

--- js/productPage.js
+++ js/productPage.js
@@ -1,24 +1,63 @@
 const sizeBtn = document.getElementById("size-dropdown");
 const infoBtn = document.getElementById("infoBtn-small");
-const infoBtnBig = document.getElementById("infoBtn")
+const infoBtnBig = document.getElementById("infoBtn");
 const infopopup = document.getElementById("information-small");
 const infopopupBig = document.getElementById("information");
+const sizeCmboBox = document.getElementById("size-dropdown");
+const sizeList = document.getElementById("size-dropdown-content");
+const addBotton = document.getElementById("addBtn");
+const colors = document.getElementById("color-list-big");
+const colorsSmall = document.getElementById("color-list");
 
 infoBtn.addEventListener("click", () => {
-  
-  if (infopopup.style.display === "block"){
+  if (infopopup.style.display === "block") {
     infopopup.style.display = "none";
-  }else{
+  } else {
     infopopup.style.display = "block";
   }
 });
 infoBtnBig.addEventListener("click", () => {
-  
-  if (infopopupBig.style.display === "block"){
+  if (infopopupBig.style.display === "block") {
     infopopupBig.style.display = "none";
-  }else{
+  } else {
     infopopupBig.style.display = "block";
   }
 });
+sizeCmboBox.addEventListener("click", () => {
+  if (sizeList.style.display === "block") {
+    sizeList.style.display = "none";
+  } else {
+    sizeList.style.display = "block";
+  }
+});
+sizeList.addEventListener("click", (event) => {
+  event.stopPropagation();
+});
 
+addBotton.addEventListener("mousedown", () => {
+  addBotton.style.backgroundColor = "rgb(68, 255, 0)";
+  addBotton.style.boxShadow = "2px 2px 10px white, -2px -2px 10px white";
+});
+addBotton.addEventListener("mouseup", () => {
+  addBotton.style.backgroundColor = "rgb(68, 255, 0, 0.90)";
+  addBotton.style.boxShadow = "none";
+});
 
+colors.addEventListener("click", (event) => {
+  if (event.target.classList.contains("color-Choice")) {
+    const allLi = colors.querySelectorAll('li');
+        allLi.forEach(li => {
+            li.classList.remove('selected');
+        });
+    event.target.classList.add('selected');
+  }
+});
+colorsSmall.addEventListener("click", (event) => {
+  if (event.target.classList.contains("color-Choice")) {
+    const allLi = colorsSmall.querySelectorAll('li');
+        allLi.forEach(li => {
+            li.classList.remove('selected');
+        });
+    event.target.classList.add('selected');
+  }
+});
